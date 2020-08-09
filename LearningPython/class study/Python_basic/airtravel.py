268eff135d26925a9be772ba4d6c53ef943f4281
jaewon Lee <jcircle67@gmail.com>
2020-08-09 09:03:21 +0900

    Update airtravel.py
    다른 언어와 파이썬의 PRIVATE, PUBLIC 차이

--- LearningPython/class study/Python_basic/airtravel.py
+++ LearningPython/class study/Python_basic/airtravel.py
@@ -4,6 +4,10 @@ class Flight:
         self._passenger_num = passenger_num
 
     def number(self):
-        return self._number
+        return self.__number
     def add_passenger(self,num):
         self._passenger_num +=num
+
+#public 용도로 클래스, 변수를 선언하여 사용할 때는 언더바를 빼고 선언하시면 된다.
+# Private 용도로 사용할 때에는 언더바를 두번 붙여서 이름을 지으시면 외부에서 참조가 불가능해진다.
+# 이는 함수에 대해서도 마찬가지로 적용이 된다
