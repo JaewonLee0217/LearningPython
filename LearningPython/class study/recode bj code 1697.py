075f1fd65aa20a4a8716c1c1c9da7be897cb0cdc
jaewon Lee <jcircle67@gmail.com>
2020-08-15 15:03:23 +0900

    Update recode bj code 1697.py

--- LearningPython/class study/recode bj code 1697.py
+++ LearningPython/class study/recode bj code 1697.py
@@ -1,7 +1,7 @@
 from collections import deque
 n,k = map(int, input().split())
 
-MAX = 1000001
+MAX = 100001
 dp = [MAX]*MAX
 dp[k] = 0
 
@@ -10,19 +10,15 @@ queue.append(k)
 
 while queue:
     num = queue.popleft()
-
     if num ==n:
         print(dp[num])
         break
     if num < MAX - 1 and dp[num + 1] > dp[num] + 1:
         dp[num + 1] = dp[num] + 1
         queue.append(num + 1)
-
     if num > 0 and dp[num - 1] > dp[num] + 1:
         dp[num - 1] = dp[num] + 1
         queue.append(num - 1)
-
-    if num % 2 == 0:
-        if dp[num // 2] > dp[num] + 1:
-            dp[num // 2] = dp[num] + 1
-            queue.append(num // 2)
+    if num % 2 == 0 and dp[num // 2] > dp[num] + 1:
+        dp[num // 2] = dp[num] + 1
+        queue.append(num // 2)
