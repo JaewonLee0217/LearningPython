869e7c6aeddb3bd191931ae021e8fa1ed6d8365d
jaewon Lee <jcircle67@gmail.com>
2020-08-30 08:23:27 +0900

    numpy& NLTK study
    numpy& NLTK study

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-print('test')
-print('hello')
-#파이썬 연습
+import numpy
+
+list_mat = [[1,2,3],[3,6,9],[2,4,6]]
+matrix = numpy.array(list_mat)
+print(matrix)
+print(matrix.shape)
+
+matrix = numpy.random.rand(3,3)
+print(matrix)
+
+matrix = numpy.zeros((3,3))
+print(matrix)
+
+numpy.savetxt("temp.txt",matrix)
+
+a=numpy.loadtxt("temp.txt")
+print(a)
