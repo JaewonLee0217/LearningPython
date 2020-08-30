77083854767b45327b1c4c1cec8caca9c0b17e6c
jaewon Lee <jcircle67@gmail.com>
2020-08-30 09:52:33 +0900

    NLTK
    NLTK

---
+++ nltk_study.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download()
+# sentence = "Hi . This is Tom . I have many cars ."
+# sentence = sentence.lower()
+# tokens = nltk.word_tokenize(sentence)
+# print(tokens)
+# text = nltk.Text(tokens)
+# print(text)
+# print(len(text.tokens))
+# print(len(set(text.tokens)))
+#                                                                                                                                                                                                                                                      for token in tokens] print(tokens)
--- test.py
+++ test.py
@@ -1,17 +1,35 @@
 import numpy
 
 list_mat = [[1,2,3],[3,6,9],[2,4,6]]
+lm = [1,2,3]
 matrix = numpy.array(list_mat)
+vec = numpy.array(lm)
+
+
+
+
+print(matrix.dot(vec))
 print(matrix)
 print(matrix.shape)
-
-matrix = numpy.random.rand(3,3)
-print(matrix)
-
-matrix = numpy.zeros((3,3))
-print(matrix)
-
+print(vec)
 numpy.savetxt("temp.txt",matrix)
 
 a=numpy.loadtxt("temp.txt")
-print(a)
+
+##############
+
+X = None
+W = None
+b = None
+
+X = numpy.array(X) # (N,D) n이 배치 사이즈고 D가 히든 디멘션
+W = numpy.array(W) # (D, O)
+b = numpy.array(b)# (O)
+
+Y = X.dot(W).add(b) # 이게 fully connected layer가 된다.
+
+# 그리디언트
+dy = numpy.array(None)
+dx = dy.dot(W.T)
+dw = X.T.dot(Y)
+db = dy.sum(1)
